1f5d476c05b8bf640c4f6c896a00d8cec5798bd1
Neintonine <iedsoftworks@gmail.com>
2022-07-31 12:46:27 +0200

    Show FPS in title

--- OpenGLRayMarching/Graphics/Window.cs
+++ OpenGLRayMarching/Graphics/Window.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
 using OpenGLRayMarching.Graphics.OpenGLBindings;
 using OpenGLRayMarching.Graphics.OpenGLBindings.Shaders;
 using OpenGLRayMarching.Graphics.Shaders;
@@ -7,12 +7,14 @@ using OpenGLRayMarching.Utils;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using DebugSeverity = OpenTK.Graphics.OpenGL4.DebugSeverity;
 
 namespace OpenGLRayMarching.Graphics;
 
 public class Window : GameWindow
 {
 
+    private const string BASE_TITLE = "Raymarching";
     private Shader basicShader;
     public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
     {
@@ -22,8 +24,18 @@ public class Window : GameWindow
     protected override void OnLoad()
     {
         GLSystem.INIT_SYSTEM();
+        GLCustomActions.AtKHRDebug += (source, type, arg3, arg4) =>
+        {
+            if (arg3 != DebugSeverity.DebugSeverityHigh)
+            {
+                throw new Exception($"Error at: {source}\nType: {type}\nSeverity: {arg3}\nMessage: {arg4}");
+            }
+            
+            Debug.WriteLine($"Error at: {source}\nType: {type}\nSeverity: {arg3}\nMessage: {arg4}");
+            
+        };
         
-        GL.ClearColor(0,0,0,1);
+        GL.ClearColor(0,1,0,1);
         basicShader = new Shader(AssemblyUtility.ReadAssemblyFile("OpenGLRayMarching.Graphics.Shaders.basic.vert"), AssemblyUtility.ReadAssemblyFile("OpenGLRayMarching.Graphics.Shaders.basic.frag"));
         
         base.OnLoad();
@@ -38,6 +50,10 @@ public class Window : GameWindow
     protected override void OnRenderFrame(FrameEventArgs args)
     {
         GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        string FPS = (1 / args.Time).ToString("F");
+        //Console.WriteLine($"Rendering: {args.Time} - {FPS}");
+        Title = $"{BASE_TITLE} - {FPS}";
         
         basicShader.Activate();
         Plate.Object.Activate();
