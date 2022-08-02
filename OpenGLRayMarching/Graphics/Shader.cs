3e06d585c63eed176378b91f99a2d1cc37b41c0c
Michel Fedde <michel.fedde@gmail.com>
2022-08-02 22:44:11 +0200

    Created custom Shader Classes

---
+++ OpenGLRayMarching/Graphics/Shader.cs
@@ -0,0 +1,28 @@
+﻿using OpenGLRayMarching.Graphics.OpenGLBindings.Shaders;
+using OpenGLRayMarching.Graphics.Static;
+using OpenGLRayMarching.Utils;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using All = OpenTK.Graphics.OpenGL4.All;
+using GL = OpenTK.Graphics.OpenGL4.GL;
+
+namespace OpenGLRayMarching.Graphics.Shaders;
+
+public abstract class Shader : GenericShader
+{
+
+    protected Shader(string combinedData) : base(combinedData)
+    { }
+    protected Shader(string vertex, string fragment) : base(vertex, fragment)
+    { }
+    protected Shader(ShaderFileCollection shaderFileFiles) : base(shaderFileFiles)
+    { }
+
+    public abstract void Render(Scene.Scene scene);
+
+    static Shader()
+    {
+        ShaderExtensions.AddAssemblyExtensions("ext", "OpenGLRayMarching.Graphics.Shaders.Extensions");
+    }
+
+}
--- OpenGLRayMarching/Graphics/Shaders/RaymarchingShader.cs
+++ OpenGLRayMarching/Graphics/Shaders/RaymarchingShader.cs
@@ -1,20 +1,18 @@
 ﻿using OpenGLRayMarching.Graphics.OpenGLBindings.Shaders;
 using OpenGLRayMarching.Graphics.Static;
+using OpenGLRayMarching.Utils;
 using OpenTK.Mathematics;
 
 namespace OpenGLRayMarching.Graphics.Shaders;
 
-public class Shader : GenericShader
+public class RaymarchingShader : Shader
 {
-
-    public Shader(string combinedData) : base(combinedData)
+    private static string _vertex = AssemblyUtility.ReadAssemblyFile("OpenGLRayMarching.Graphics.Shaders.Files.raymarching.vert");
+    private static string _fragment = AssemblyUtility.ReadAssemblyFile("OpenGLRayMarching.Graphics.Shaders.Files.raymarching.frag");
+    
+    public RaymarchingShader() : base(_vertex, _fragment)
     { }
-    public Shader(string vertex, string fragment) : base(vertex, fragment)
-    { }
-    public Shader(ShaderFileCollection shaderFileFiles) : base(shaderFileFiles)
-    { }
-
-    public void Render(Scene.Scene scene)
+    public override void Render(Scene.Scene scene)
     {
         scene.Camera.GetMatrix(out Matrix4 mvp, out Matrix4 model);
         Activate();
@@ -24,7 +22,8 @@ public class Shader : GenericShader
         Uniforms["u_mvp"].SetMatrix4(mvp);
         Uniforms["u_cameraPosition"].SetVector3(scene.Camera.Position);
         
+        Uniforms["u_envTexture"].SetTexture(scene.EnviromentTexture);
+        
         DrawObject(Plate.Object);
     }
-
 }
